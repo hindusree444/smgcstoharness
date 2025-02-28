30479a76f3de25d1c142332d2c27cbae00c1f634
hindu sree <hdunaboyina@softility.com>
2025-02-28 18:45:43 +0530

    adding

---
+++ main.tf
@@ -0,0 +1,35 @@
+terraform {
+  backend "s3" {
+    bucket         = "stgcpharnesss"            
+    key            = "terraform/statefile/terraform.tfstate"  
+    region         = "us-central1"                      
+    encrypt        = true                              
+  }
+}
+provider "google" {
+
+  project     = "devops-e2e-workflow"                               
+  region      = "us-central1"                                     
+}
+
+
+resource "google_compute_instance" "vm_instance" {
+  name         = "my-gcp-vm"
+  machine_type = "e2-medium"                  
+  zone         = "us-central1-a"              
+
+  boot_disk {
+    initialize_params {
+      image = "debian-9-stretch-v20210114"     
+    }
+  }
+
+  network_interface {
+    network = "default"
+    access_config {  # Allocate a public IP
+    }
+  }
+
+}
+
+
