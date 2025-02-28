6671c1b76b1233ed59350698dc84a2810a16fb52
hindusree444 <134682745+hindusree444@users.noreply.github.com>
2025-02-28 19:51:10 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,25 +1,27 @@
 terraform {
   backend "gcs" {
-    bucket         = "stgcpharnesss1"            
-    region         = "us-central1"                      
-    encrypt        = true                              
+    bucket         = "stgcpharnesss1"   # Ensure this bucket exists
+    prefix         = "terraform/state"  # Optional, but helps in organizing state
+    region         = "us-central1"
+    encrypt        = true
+    # Optional: state locking, if you want to add locking mechanism, you can add Firestore as a lock backend
   }
 }
+
 provider "google" {
-
-  project     = "statemigration"                               
-  region      = "us-central1"                                     
+  project     = "statemigration"       # Ensure this project exists
+  region      = "us-central1"          # Ensure this region is correct
+  credentials = file("<PATH_TO_YOUR_SERVICE_ACCOUNT_KEY_JSON>")  # Harness service account credentials
 }
-
 
 resource "google_compute_instance" "vm_instance" {
   name         = "my-gcp-vm"
-  machine_type = "e2-medium"                  
-  zone         = "us-central1-a"              
+  machine_type = "e2-medium"
+  zone         = "us-central1-a"
 
   boot_disk {
     initialize_params {
-      image = "debian-9-stretch-v20210114"     
+      image = "debian-9-stretch-v20210114"
     }
   }
 
@@ -28,7 +30,4 @@ resource "google_compute_instance" "vm_instance" {
     access_config {  # Allocate a public IP
     }
   }
-
 }
-
-
